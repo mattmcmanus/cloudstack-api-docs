1fd7a97704d2284948d3739de1d0c93a86d37b15
Matt McManus <matt@mcmanus.io>
2013-10-04 23:53:29 -0400

    Initial commit

---
+++ generate-docs.js
@@ -0,0 +1,11 @@
+var jade = require('jade');
+var fs = require('fs');
+
+var data = fs.readFileSync('listApis.json', { encoding:'utf8' });
+
+data = JSON.parse(data)
+
+// renderFile
+var html = jade.renderFile('documentation.jade', data)
+
+fs.writeFileSync("api.html", html)
